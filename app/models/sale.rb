a3980fcccbb172c2c3fe0f1b6b3232de18bcc0d9
Hakeem <babahakeem@outlook.com>
2021-02-22 16:17:56 +0100

    country select gem added

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,3 +1,9 @@
 class Sale < ApplicationRecord
   belongs_to :user
+
+  def country_name
+    nation = ISO3166::Country[customer_country]
+    nation.translations[I18n.locale.to_s] || nation.name
+  end
+
 end
