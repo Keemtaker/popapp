e9eb32663c15bf057af3fb36a0beaf35cc9f79cd
Hakeem <babahakeem@outlook.com>
2021-02-26 15:15:36 +0100

    retrive sales method in user model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :callback ]
 
   def home
+    @api_permission = 'MERCHANT_PROFILE_READ+CUSTOMERS_READ+ORDERS_READ+ITEMS_READ+ORDERS_WRITE+PAYMENTS_WRITE'
     recent_sales = Sale.all.limit(10).order("id DESC")
     @sale = recent_sales.sample
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,65 @@
 class User < ApplicationRecord
+  after_create :retrieve_sales
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :sales, dependent: :destroy
+
+  def retrieve_sales
+    $merchant_client = Square::Client.new(
+      access_token: self.access_token,
+      environment: ENV['SQUARE_ENVIRONMENT']
+    )
+
+    merchant_results = $merchant_client.merchants.retrieve_merchant(
+      merchant_id: self.merchant_id
+    )
+
+    results = $merchant_client.orders.search_orders(
+    body: {
+      location_ids: [
+        merchant_results.data["merchant"][:main_location_id]
+      ],
+      query: {
+        filter: {
+          state_filter: {
+            states: [
+              "COMPLETED"
+            ]
+          }
+        }
+      }
+    }
+  )
+
+    if results.data.present?
+      completed_orders = results.data["orders"].select { |order| order[:customer_id].present? }
+      if completed_orders.present?
+        new_sale_array = []
+        completed_orders.each do | order |
+          customer_details = $merchant_client.customers.retrieve_customer(
+          customer_id: order[:customer_id]
+        )
+
+          new_sale = Sale.new(user_id: self.id, customer_name: customer_details.data["customer"][:given_name], customer_locality: customer_details.data["customer"][:address][:locality],
+                          customer_country: customer_details.data["customer"][:address][:country], sale_created_at: order[:created_at], sale_updated_at: order[:updated_at])
+
+          order[:line_items].each do | line_item |
+            new_sale_array << {"catalog_object_id" => line_item[:catalog_object_id], "name" => line_item[:name], "price_total" => line_item[:total_money][:amount],
+            "price_currency" => line_item[:total_money][:currency]}
+          end
+          new_sale.line_items = new_sale_array
+          new_sale.save!
+          new_sale_array = []
+        end
+      end
+    end
+  end
+
+  private
 
   def email_required?
      false
