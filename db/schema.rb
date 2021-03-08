6c8dd9b3852e15103830c65c809ea66918a1ffaf
Hakeem <babahakeem@outlook.com>
2021-03-08 21:36:10 +0100

    slug set for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_130640) do
+ActiveRecord::Schema.define(version: 2021_03_08_201757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_02_25_130640) do
     t.string "merchant_id"
     t.string "refresh_token"
     t.string "access_token_expires_at"
+    t.string "slug"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
