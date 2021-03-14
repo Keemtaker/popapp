d4789ca2a178ee16ee2f9335d3237a389126ebe2
Hakeem <babahakeem@outlook.com>
2021-03-14 14:14:56 +0100

    navbar setup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :callback ]
 
   def home
-    @api_permission = 'MERCHANT_PROFILE_READ+CUSTOMERS_READ+ORDERS_READ+ITEMS_READ+ORDERS_WRITE+PAYMENTS_WRITE'
     # recent_sales = Sale.all.limit(10).order("id DESC")
     # @sale = recent_sales.sample
 
