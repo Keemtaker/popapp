fbabc395be3d824dddcf207fce2f61c27f3c1928
Hakeem <babahakeem@outlook.com>
2021-03-16 19:20:55 +0100

    sort migration files for heroku

--- db/migrate/20210225130212_remove_access_token_to_sales.rb
+++
@@ -1,8 +0,0 @@
-class RemoveAccessTokenToSales < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :sales, :access_token, :string
-    remove_column :sales, :merchant_id, :string
-    remove_column :sales, :refresh_token, :string
-    remove_column :sales, :access_token_expires_at, :string
-  end
-end
